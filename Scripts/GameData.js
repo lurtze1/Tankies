b5f7e28579da291a8fb125614239e166d4bbced9
lurtze1 <lurtze1@gmail.com>
2016-03-21 13:21:59 +0100

    Stopped the calling of logs every tick

--- Scripts/GameData.js
+++ Scripts/GameData.js
@@ -44,6 +44,10 @@ var Tank = function (pos) {
     //Indicates if this object cannot be moved
     this.heavy = false;
 
+    this.width = 40;
+
+    this.height = 40;
+
     //Polygon holds angle, position, size, offset.
     this.polygon = P(V(pos), [V(20, 20), V(-20, 20), V(-20, -20), V(20, -20), V(20, 0)]);
 
@@ -79,18 +83,135 @@ var Wall = function (pos) {
     //Indicates if this object cannot be moved
     this.heavy = true;
 
+    this.width = 20;
+
+    this.height = 20;
+
     //Polygon holds angle, postion, size, offset.
-    this.polygon = P(V(pos), [V(20, 20), V(-20, 20), V(-20, -20), V(20, -20), V(20, 0)]);
+    this.polygon = P(V(pos), [V(10, 10), V(-10, 10), V(-10, -10), V(10, -10), V(10, 0)]);
+};
+
+Wall.prototype = {
+    respondtoCollision: function (other, response) {
+        if (this.solid && other.solid && !(this.heavy && other.heavy)) {
+            if (this.heavy) {
+                // Move the other object out of us
+                other.pos.add(response.overlapV);
+            } else if (other.heavy) {
+                // Move us out of the other object
+                this.pos.sub(response.overlapV);
+            } else {
+                // Move equally out of each other
+                response.overlapV.scale(0.5);
+                this.pos.sub(response.overlapV);
+                other.pos.add(response.overlapV);
+            }
+        }
+    }
 };
 
 var game = function () {
+    addEventListener("keydown", function (e) {
+        keysDown[e.keyCode] = true;
+    }, false);
+
+    addEventListener("keyup", function (e) {
+        delete keysDown[e.keyCode];
+    }, false);
+
+
+    var addPlayer = function () {
+        if (playerList[0] == undefined) {
+            LocalPlayer = new Tank(V(50, 50));
+            entities += LocalPlayer;
+        } else if (playerList[1] == undefined) {
+            LocalPlayer = new Tank(V(950, 950));
+            entities += LocalPlayer;
+        } else if (playerList[2] == undefined) {
+            LocalPlayer = new Tank(V(50, 950));
+            entities += LocalPlayer;
+        } else if (playerList[3] == undefined) {
+            LocalPlayer = new Tank(V(950, 50));
+            entities += LocalPlayer;
+        }
+    };
+
+    var reset = function () {
+        LocalPlayer.pos.x =
+            canvas.width / 2;
+        LocalPlayer.pos.y = canvas.height / 2;
+    };
+
+
+    var Collision = function (loopCount) {
+        var response;
+        for (var i = 0; i < loopCount; i++) {
+            // Naively check for collision between all pairs of entities
+            // E.g if there are 4 entities: (0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)
+            for (var aCount = 0; aCount < entities.length; aCount++) {
+                var a = entities[aCount];
+                for (var bCount = aCount + 1; bCount < entities.length; bCount++) {
+                    var b = entities[bCount];
+                    if (response) {
+                        response.clear();
+
+                    }
+                    var collided;
+                    var aData = a.polygon;
+                    var bData = b.polygon;
+                    collided = SAT.testPolygonPolygon(aData, bData, response);
+                }
+                if (collided) {
+                    a.respondToCollision(b, response);
+                }
+            }
+        }
+    };
+
+    var update = function (modifier) {
+        if (87 in keysDown) { // Player holding up
+            LocalPlayer.pos.y -= LocalPlayer.speed * modifier;
+        }
+        if (83 in keysDown) { // Player holding down
+            LocalPlayer.pos.y += LocalPlayer.speed * modifier;
+        }
+        if (65 in keysDown) { // Player holding left
+            LocalPlayer.pos.x -= LocalPlayer.speed * modifier;
+        }
+        if (68 in keysDown) { // Player holding right
+            LocalPlayer.pos.x += LocalPlayer.speed * modifier;
+        }
+
+    };
+
+    var render = function () {
+        if (bgReady) {
+            ctx.drawImage(bgImage, 0, 0);
+        }
+        if (TankReady) {
+            ctx.drawImage(TankImage, LocalPlayer.pos.x, LocalPlayer.pos.y, LocalPlayer.width, LocalPlayer.height)
+        }
+    };
+
+    var Start = function () {
+        var now = Date.now();
+        var delta = now - then;
+        update(delta / 1000);
+        render();
+
+        then = now;
+        requestAnimationFrame(Start);
+    };
+
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
     canvas.width = 1000;
     canvas.height = 1000;
     document.body.appendChild(canvas);
-
+    var LocalPlayer;
     var entities = [];
+    var playerList = [];
+    addPlayer();
     var bgReady = false;
     var bgImage = new Image(500, 500);
     bgImage.onload = function () {
@@ -108,91 +229,11 @@ var game = function () {
     };
     TankImage.src = "Images/Tank.png";
 
-    var tank = new Tank(V(canvas.width / 2, canvas.height / 2));
+
     var keysDown = {};
 
-    addEventListener("keydown", function (e) {
-        keysDown[e.keyCode] = true;
-    }, false);
-
-    addEventListener("keyup", function (e) {
-        delete keysDown[e.keyCode];
-    }, false);
-
-
-    var reset = function () {
-        tank.pos.x = canvas.width / 2;
-        tank.pos.y = canvas.height / 2;
-    };
-
-
-    var Collision = function (loopCount) {
-        var loopCount = loopCount;
-        for (var i = 0; i < loopCount; i++) {
-            // Naively check for collision between all pairs of entities
-            // E.g if there are 4 entities: (0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)
-            for (var aCount = 0; aCount < entities.length; aCount++) {
-                var a = entities[aCount];
-                for (var bCount = aCount + 1; bCount < entities.length; bCount++) {
-                    var b = entities[bCount];
-                    this.response.clear();
-                    var collided;
-                    var aData = a.data;
-                    var bData = b.data;
-                    collided = SAT.testPolygonPolygon(aData, bData, this.response);
-                }
-                if (collided) {
-                    a.respondToCollision(b, this.response);
-                }
-            }
-        }
-    };
-
-    var update = function (modifier) {
-        if (87 in keysDown) { // Player holding up
-            tank.pos.y -= tank.speed * modifier;
-        }
-        if (83 in keysDown) { // Player holding down
-            tank.pos.y += tank.speed * modifier;
-        }
-        if (65 in keysDown) { // Player holding left
-            tank.pos.x -= tank.speed * modifier;
-        }
-        if (68 in keysDown) { // Player holding right
-            tank.pos.x += tank.speed * modifier;
-        }
-
-
-    };
-
-    var render = function () {
-        if (bgReady) {
-            ctx.drawImage(bgImage, 0, 0);
-        }
-        if (TankReady) {
-            ctx.drawImage(TankImage, tank.pos.x, tank.pos.y)
-        }
-
-        ctx.fillStyle = "rgb(250, 250, 250)";
-        ctx.font = "24px Helvetica";
-        ctx.textAlign = "left";
-        ctx.textBaseline = "top";
-        ctx.fillText("henk", 40, 40);
-    };
-
-    var Start = function () {
-        var now = Date.now();
-        var delta = now - then;
-        update(delta / 1000);
-        render();
-
-        then = now;
-        requestAnimationFrame(Start);
-    };
-
-
     var then = Date.now();
-
-    reset();
     Start();
 };
+
+game();
