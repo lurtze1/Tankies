d59231610feb5bd15017eb49642abc9701bdac00
Youri van Dorp <s1081975@student.windesheim.nl>
2016-04-01 15:50:44 +0200

    SPA

---
+++ Scripts/PageController.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Youri van Dorp on 3/31/2016.
+ */
+var PageApp = angular.module('PageApp', ['ngRoute']);
+
+PageApp.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl : 'pages/MainMenu.html',
+            controller  : 'MainMenuController'
+        })
+
+        .when('/HowToPlay', {
+            templateUrl : 'pages/HowToPlay.html',
+            controller  : 'HowToPlayController'
+        })
+
+        .when('/Credits', {
+            templateUrl : 'pages/Credits.html',
+            controller  : 'CreditsController'
+        })
+
+        .otherwise({redirectTo: '/'});
+});
+
+PageApp.controller('MainMenuController', function($scope) {
+
+});
+
+PageApp.controller('HowToPlayController', function($scope) {
+
+});
+
+PageApp.controller('CreditsController', function($scope) {
+
+});
